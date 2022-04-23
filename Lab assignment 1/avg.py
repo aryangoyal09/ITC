9a49c1b408bb99258eb49c7916c71d190712d182
aryangoyal09 <104254279+aryangoyal09@users.noreply.github.com>
2022-04-23 18:49:52 +0530

    Create avg.py

---
+++ Lab assignment 1/avg.py
@@ -0,0 +1,4 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+print("The Average is:" , ((num1 + num2 + num3) / 3))
