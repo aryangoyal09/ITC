93663b517f5ad37a6c9b6d6dd761ef7a93c16f9d
aryangoyal09 <104254279+aryangoyal09@users.noreply.github.com>
2022-04-24 16:32:57 +0530

    Add files via upload

---
+++ Lab assignment 1/list.py
@@ -0,0 +1,24 @@
+SID = int(input('Enter SID: '))
+Name = str(input('Enter name: '))
+
+Gender = str(input('Enter Gender(F, M, or U for Unknown): '))
+
+# Gender constraints
+while Gender not in ('F', 'M', 'U'):
+    print('Invalid Gender Value, try again\n')
+    Gender = str(input('Enter Gender(F, M or U for Unknown): '))
+else:
+    pass
+
+Course_Name = str(input('Enter Course_Name: '))
+CGPA = float(input('Enter CGPA: '))
+
+# CGPA constraints
+while CGPA>10 or CGPA<0:
+    print('Invalid CGPA Value, try again\n')
+    CGPA = float(input('Enter CGPA: '))
+else:
+    pass
+
+Student = [SID,Name,Gender,Course_Name,CGPA]
+print('Your data is:',Student)
---
+++ Lab assignment 1/sorted_list.py
@@ -0,0 +1,10 @@
+# Input from user
+Marks_A = float(input('Marks of A: '))
+Marks_B = float(input('Marks of B: '))
+Marks_C = float(input('Marks of C: '))
+Marks_D = float(input('Marks of D: '))
+Marks_E = float(input('Marks of E: '))
+
+Marks = [Marks_A,Marks_B,Marks_C,Marks_D,Marks_E]
+Marks.sort()
+print('The sorted mark list is:',Marks)
