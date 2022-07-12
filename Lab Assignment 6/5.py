b92cb6c4408ffaa03a3091ab32639a3f34e69cda
aryangoyal09 <104254279+aryangoyal09@users.noreply.github.com>
2022-07-12 07:56:24 +0000

    Update 5.py

--- Lab Assignment 6/5.py
+++ Lab Assignment 6/5.py
@@ -1,10 +1,14 @@
-# Taking and splitting the input by hyphen
-list = str(input('Enter words separated by hyphen: ')).split('-')
-list.sort()
-output = ''
+# Defining function
+def sorting(list):
+    list.sort()
+    output = ''
 
-for i in list:
-    output = output + '-' + i
+    for i in list:
+        output = output + '-' + i
 
 # We need to ignore the first hyphen
-print(output[1::])
+    print(output[1::])
+
+# Input list
+list = str(input('Enter words separated by hyphen: ')).split('-')
+sorting(list)
