60c077ea694dd15825a12f1d204c2ab3741f35c9
aryangoyal09 <104254279+aryangoyal09@users.noreply.github.com>
2022-07-12 07:49:52 +0000

    Update 4.py

--- Lab Assignment 6/4.py
+++ Lab Assignment 6/4.py
@@ -1,10 +1,14 @@
-line = str(input('Enter string to check for: '))
-alphabet='abcdefghijklmnopqrstuvwxyz'
+def panagram(line):
+    alphabet='abcdefghijklmnopqrstuvwxyz'
 
-for i in alphabet:
-    if i in line.lower():
-        continue
-    print('Not a panagram')
-    break
-else:
-    print('Panagram!')
+    for i in alphabet:
+        if i in line.lower():
+            continue
+        print('Not a panagram')
+        break
+    else:
+        print('Panagram!')
+
+# Asking user for input line
+line = str(input('Enter string to check for: '))
+panagram(line)
