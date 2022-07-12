902fc9f37a65e709a15ea1061967f22d771aff36
aryangoyal09 <104254279+aryangoyal09@users.noreply.github.com>
2022-07-12 07:53:36 +0000

    Update 2.py

--- Lab Assignment 6/2.py
+++ Lab Assignment 6/2.py
@@ -1,9 +1,13 @@
-# Input line:
-line = str(input('Enter string here: '))
-reverse = line[::-1]
+# Defining function
+def palindrome(line):
+    reverse = line[::-1]
 
-# As seen in the example "Nurses run", the program should ignore whitespaces and capitals
-if line.lower().replace(' ', '') == reverse.lower().replace(' ', ''):
-    print('Palindrome!')
-else:
-    print('Not a palindrome')
+    # As seen in the example "Nurses run", the program should ignore whitespaces and capitals
+    if line.lower().replace(' ', '') == reverse.lower().replace(' ', ''):
+        print('Palindrome!')
+    else:
+        print('Not a palindrome')
+
+# Input line
+line = str(input('Enter string here: '))
+palindrome(line)
