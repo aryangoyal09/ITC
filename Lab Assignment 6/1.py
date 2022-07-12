16194f7fae5145dbe06b7cb034de904157eb06ac
aryangoyal09 <104254279+aryangoyal09@users.noreply.github.com>
2022-07-12 07:49:04 +0000

    Add files via upload

---
+++ Lab Assignment 6/1.py
@@ -0,0 +1,18 @@
+# Defining function for perfect number
+def perfect(n):
+    sum_of_divisors = 0
+
+# Calculating sum of divisors of the input number
+    for i in range(1, n+1):
+        if n%i==0:
+            sum_of_divisors = sum_of_divisors + i
+
+    if n==0:
+        print('Number is not perfect')
+    elif n == sum_of_divisors/2:
+        print('Number is perfect')
+    else:
+        print('Number is not perfect')
+
+n = int(input('Enter number: '))
+perfect(n)
