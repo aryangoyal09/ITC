00088c7c574a6993566e2d6a037b51e95d1b257b
aryangoyal09 <104254279+aryangoyal09@users.noreply.github.com>
2022-07-16 23:57:37 +0530

    Create 2.py

---
+++ Lab Assignment 7/2.py
@@ -0,0 +1,27 @@
+from tkinter import *
+from calendar import *
+
+root = Tk()
+root.title("Calendar")
+
+# Creating year entry
+Label1 = Label(root, text="Enter year")
+Year_entry = Entry(root)
+
+# Packing Entries and Labels
+Label1.pack()
+Year_entry.pack()
+
+def print_calendar():
+    Year = int(Year_entry.get())
+    Calendar_of_year = calendar(Year)
+    textbox.delete(0.0, END)
+    textbox.insert(INSERT, Calendar_of_year)
+
+Button1 = Button(root, text = "Show", command=print_calendar)
+Button1.pack()
+
+textbox = Text(root)
+textbox.pack()
+
+root.mainloop()
