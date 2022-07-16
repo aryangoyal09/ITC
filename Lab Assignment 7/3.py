dc6f44f74a406513302b41917dd879d50c3541c9
aryangoyal09 <104254279+aryangoyal09@users.noreply.github.com>
2022-07-17 01:33:59 +0530

    Update 3.py

--- Lab Assignment 7/3.py
+++ Lab Assignment 7/3.py
@@ -1,94 +1,106 @@
 from tkinter import *
 
-expression = ""
-def press(num):
-	global expression
-	expression = expression + str(num)
-	equation.set(expression)
+root = Tk()
+root.title('Calculator')
 
-def equalpress():
-	try:
+# Creating and placing entry
+entry_box = Entry(root, width=30)
+entry_box.grid(row=0, column=0, columnspan=4)
 
-		global expression
-		total = str(eval(expression))
-		equation.set(total)
-		expression = ""
-	except:
-		equation.set(" error ")
-		expression = ""
+# defining Functions
+def number_click(num):
+    entry_box.insert(END, num)
 
-def clear():
-	global expression
-	expression = ""
-	equation.set("")
+def clear_click():
+    entry_box.delete(0, END)
 
+def plus_click():
+    global num1
+    global math
+    math="+"
+    num1 = entry_box.get() 
+    num1 = float(num1)
+    entry_box.delete(0, END)
 
-my_calculator = Tk()
+def minus_click():
+    global num1
+    global math
+    math="-"
+    num1 = entry_box.get() 
+    num1 = float(num1)
+    entry_box.delete(0, END)
 
-my_calculator.title("Calculator")
-my_calculator.geometry("270x150")
+def multiply_click():
+    global num1
+    global math
+    math="x"
+    num1 = entry_box.get() 
+    num1 = float(num1)
+    entry_box.delete(0, END)
 
-equation = StringVar()
-expression_field = Entry(my_calculator, textvariable=equation) # takes input of operation
-expression_field.grid(columnspan=4, ipadx=70)
+def divide_click():
+    global num1
+    global math
+    math="/"
+    num1 = entry_box.get() 
+    num1 = float(num1)
+    entry_box.delete(0, END)
 
-# buttons and their commands
-button1 = Button(my_calculator, text=' 1 ', command=lambda: press(1), height=1, width=7)
-button1.grid(row=2, column=0)
+def equals_click():
+    global num2
+    num2 = entry_box.get()
+    num2 = float(num2)
+    if math == "+":
+        entry_box.delete(0, END)
+        entry_box.insert(0, num1+num2)
+    elif math == "-":
+        entry_box.delete(0, END)
+        entry_box.insert(0, num1-num2)
+    elif math == "x":
+        entry_box.delete(0, END)
+        entry_box.insert(0, num1*num2)
+    elif math == "/":
+        entry_box.delete(0, END)
+        entry_box.insert(0, num1/num2)
 
-button2 = Button(my_calculator, text=' 2 ', command=lambda: press(2), height=1, width=7)
-button2.grid(row=2, column=1)
+# Creating buttons and entries
+button_1 = Button(root, text="1", padx=20, pady=20, command=lambda: number_click(1))
+button_2 = Button(root, text="2", padx=20, pady=20, command=lambda: number_click(2))
+button_3 = Button(root, text="3", padx=20, pady=20, command=lambda: number_click(3))
+button_4 = Button(root, text="4", padx=20, pady=20, command=lambda: number_click(4))
+button_5 = Button(root, text="5", padx=20, pady=20, command=lambda: number_click(5))
+button_6 = Button(root, text="6", padx=20, pady=20, command=lambda: number_click(6))
+button_7 = Button(root, text="7", padx=20, pady=20, command=lambda: number_click(7))
+button_8 = Button(root, text="8", padx=20, pady=20, command=lambda: number_click(8))
+button_9 = Button(root, text="9", padx=20, pady=20, command=lambda: number_click(9))
+button_0 = Button(root, text="0", padx=20, pady=20, command=lambda: number_click(0))
+button_plus = Button(root, text="+", padx=20, pady=20, command=plus_click)
+button_minus = Button(root, text="-", padx=20, pady=20, command=minus_click)
+button_divide = Button(root, text="/", padx=20, pady=20, command=divide_click)
+button_multiply = Button(root, text="x", padx=20, pady=20, command=multiply_click)
+button_equals = Button(root, text="=", padx=20, pady=20, command=equals_click)
+button_clear = Button(root, text="Clear", padx=8, pady=20, command=clear_click)
 
-button3 = Button(my_calculator, text=' 3 ', command=lambda: press(3), height=1, width=7)
-button3.grid(row=2, column=2)
+# Place buttons
+button_1.grid(row=3, column=0)
+button_2.grid(row=3, column=1)
+button_3.grid(row=3, column=2)
 
-button4 = Button(my_calculator, text=' 4 ', command=lambda: press(4), height=1, width=7)
-button4.grid(row=3, column=0)
+button_4.grid(row=2, column=0)
+button_5.grid(row=2, column=1)
+button_6.grid(row=2, column=2)
 
-button5 = Button(my_calculator, text=' 5 ', command=lambda: press(5), height=1, width=7)
-button5.grid(row=3, column=1)
+button_7.grid(row=1, column=0)
+button_8.grid(row=1, column=1)
+button_9.grid(row=1, column=2)
 
-button6 = Button(my_calculator, text=' 6 ', command=lambda: press(6), height=1, width=7)
-button6.grid(row=3, column=2)
+button_0.grid(row=4, column=0)
 
-button7 = Button(my_calculator, text=' 7 ', command=lambda: press(7), height=1, width=7)
-button7.grid(row=4, column=0)
+button_plus.grid(row=1, column=3)
+button_minus.grid(row=2, column=3)
+button_multiply.grid(row=3, column=3)
+button_divide.grid(row=4, column=3)
+button_clear.grid(row=4, column=1)
+button_equals.grid(row=4, column=2)
 
-button8 = Button(my_calculator, text=' 8 ', command=lambda: press(8), height=1, width=7)
-button8.grid(row=4, column=1)
-
-button9 = Button(my_calculator, text=' 9 ', command=lambda: press(9), height=1, width=7)
-button9.grid(row=4, column=2)
-
-button0 = Button(my_calculator, text=' 0 ', command=lambda: press(0), height=1, width=7)
-button0.grid(row=5, column=0)
-
-plus = Button(my_calculator, text=' + ', bg='blue',
-				command=lambda: press("+"), height=1, width=7)
-plus.grid(row=2, column=3)
-
-minus = Button(my_calculator, text=' - ', bg='blue',
-				command=lambda: press("-"), height=1, width=7)
-minus.grid(row=3, column=3)
-
-multiply = Button(my_calculator, text=' * ', bg='blue',
-					command=lambda: press("*"), height=1, width=7)
-multiply.grid(row=4, column=3)
-
-divide = Button(my_calculator, text=' / ', bg='blue',
-					command=lambda: press("/"), height=1, width=7)
-divide.grid(row=5, column=3)
-
-equal = Button(my_calculator, text=' = ', bg='yellow',
-				command=equalpress, height=1, width=7)
-equal.grid(row=5, column=2)
-
-clear = Button(my_calculator, text='Clear', bg='red',
-				command=clear, height=1, width=7)
-clear.grid(row=5, column='1')
-
-Decimal= Button(my_calculator, text='.', bg='green',
-					command=lambda: press('.'), height=1, width=7)
-Decimal.grid(row=6, column=0)
-	
-my_calculator.mainloop()
+root.mainloop()
